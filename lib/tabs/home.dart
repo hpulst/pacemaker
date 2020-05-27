07159d5a0b833bec05b925bb9ed683a776113f14
Naskapie <naskapie@gmx.net>
2020-05-27 15:55:54 +0200

    Delete Sliver

--- lib/tabs/home.dart
+++ lib/tabs/home.dart
@@ -11,65 +11,30 @@ class HomePage extends StatelessWidget {
     return DefaultTabController(
       length: myTabs.length,
       child: Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              title: Text('Marathon in 3:15 Std'),
-              centerTitle: true,
-              bottom: TabBar(
-                tabs: myTabs,
-              ),
-              floating: true,
-              // flexibleSpace: Placeholder(),
-              // expandedHeight: 20,
-            ),
-            SliverFixedExtentList(
-              itemExtent: 300,
-              delegate: SliverChildListDelegate(
-                // [
-                //   Container(color: Colors.red),
-                //   Container(color: Colors.purple),
-                //   Container(color: Colors.green),
-                //   Container(color: Colors.orange),
-                //   Container(color: Colors.yellow),
-                //   Container(color: Colors.pink),
-                // ],
-
-                [
-                  ListView(children: [
-                    MakeListTile(
-                        'Mo.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
-                    MakeListTile('Di.', 'Ruhetag', '', ''),
-                    MakeListTile(
-                        'Mi.', '13km', '59:30 min  4:35 min/km', '85-90%'),
-                    MakeListTile(
-                        'Do.', '15km', '1:25 Std   5:40 min/km', '75-80%'),
-                    MakeListTile('Fr.', 'Ruhetag', '', ''),
-                    MakeListTile('Sa.', '3 x 2 km in 8:10 min Trabpause 5 min',
-                        '5:10 min/km', '90-95%'),
-                    MakeListTile(
-                        'So.', '15km', '1:18 Std   5:20 min/km', '75-80%'),
-                    MakeListTile(
-                        'Mo.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
-                    MakeListTile(
-                        'Di.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
-                    MakeListTile(
-                        'Mi.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
-                    MakeListTile(
-                        'Do.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
-                    MakeListTile(
-                        'Fr.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
-                    MakeListTile(
-                        'Sa.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
-                    MakeListTile(
-                        'So.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
-                  ]),
-                ],
-              ),
-              // ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Marathon in 3:15 Std'),
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: myTabs,
+          ),
         ),
+        body: ListView(children: [
+          MakeListTile('Mo.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
+          MakeListTile('Di.', 'Ruhetag', '', ''),
+          MakeListTile('Mi.', '13km', '59:30 min  4:35 min/km', '85-90%'),
+          MakeListTile('Do.', '15km', '1:25 Std   5:40 min/km', '75-80%'),
+          MakeListTile('Fr.', 'Ruhetag', '', ''),
+          MakeListTile('Sa.', '3 x 2 km in 8:10 min Trabpause 5 min',
+              '5:10 min/km', '90-95%'),
+          MakeListTile('So.', '15km', '1:18 Std   5:20 min/km', '75-80%'),
+          MakeListTile('Mo.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
+          MakeListTile('Di.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
+          MakeListTile('Mi.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
+          MakeListTile('Do.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
+          MakeListTile('Fr.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
+          MakeListTile('Sa.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
+          MakeListTile('So.', '15km', '1:18 Std   5:10 min/km', '75-80%'),
+        ]),
       ),
     );
   }

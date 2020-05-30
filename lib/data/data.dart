c20971ff45836880276d40ec58c8fe5d251e83d4
Naskapie <naskapie@gmx.net>
2020-05-31 01:36:59 +0200

    Database Model

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
 
+import 'package:Pacemaker/model/model.dart';
 import 'package:flutter/widgets.dart';
 
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DataTable {
-  // Define a function that inserts dogs into the database
+  // Define a function that inserts workouts into the database
   Future<void> insertTimeTable(DataTable dataTable) async {
     // Avoid errors caused by flutter upgrade.
     // Importing 'package:flutter/widgets.dart' is required.
@@ -15,11 +16,11 @@ class DataTable {
       // Set the path to the database. Note: Using the `join` function from the
       // `path` package is best practice to ensure the path is correctly
       // constructed for each platform.
-      join(await getDatabasesPath(), 'doggie_database.db'),
-      // When the database is first created, create a table to store dogs.
+      join(await getDatabasesPath(), 'workout_database.db'),
+      // When the database is first created, create a table to store workouts.
       onCreate: (db, version) {
         return db.execute(
-          "CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)",
+          "CREATE TABLE workouts(id INTEGER PRIMARY KEY, weekday TEXT, km TEXT, time TEXT, pace TEXT, intensity TEXT, heartrate TEXT)",
         );
       },
       // Set the version. This executes the onCreate function and provides a
@@ -27,16 +28,16 @@ class DataTable {
       version: 1,
     );
 
-    Future<void> insertDog(Dog dog) async {
+    Future<void> insertWorkout(Workout workout) async {
       // Get a reference to the database.
       final Database db = await database;
 
-      // Insert the Dog into the correct table. Also specify the
-      // `conflictAlgorithm`. In this case, if the same dog is inserted
+      // Insert the workout into the correct table. Also specify the
+      // `conflictAlgorithm`. In this case, if the same workout is inserted
       // multiple times, it replaces the previous data.
       await db.insert(
-        'dogs',
-        dog.toMap(),
+        'workouts',
+        workout.toMap(),
         conflictAlgorithm: ConflictAlgorithm.replace,
       );
     }
--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,11 +1,35 @@
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+
 class Workout {
   final int id;
   final String weekday;
   final String km;
-  String time;
-  String pace;
-  String intensity;
-  String heartrate;
+  final String time;
+  final String pace;
+  final String intensity;
+  final String heartrate;
 
-  Workout({this.id, this.weekday, this.km});
+  Workout(
+      {this.id,
+      this.weekday,
+      this.km,
+      this.time,
+      this.pace,
+      this.intensity,
+      this.heartrate});
+
+  // Convert a Workout into a Map. The keys must correspond to the names of the
+  // columns in the database.
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'weekday': weekday,
+      'km': km,
+      'time': time,
+      'pace': pace,
+      'intensity': intensity,
+      'heartrate': heartrate,
+    };
+  }
 }

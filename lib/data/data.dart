0b89c869116b7e036c3539f4df9efd864d236d38
Naskapie <naskapie@gmx.net>
2020-05-30 18:22:29 +0200

    Database Modeld

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,29 +1,44 @@
 import 'dart:async';
 
+import 'package:flutter/widgets.dart';
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-class TimeTable {
-  final weekDay;
-  final distance;
-  final time;
-  final heartRate;
-
-  TimeTable({this.weekDay, this.distance, this.time, this.heartRate});
-
+class DataTable {
   // Define a function that inserts dogs into the database
-  Future<void> insertTimeTable(TimeTable timeTable) async {
-    // Get a reference to the database.
-    // final Database db = await database;
+  Future<void> insertTimeTable(DataTable dataTable) async {
+    // Avoid errors caused by flutter upgrade.
+    // Importing 'package:flutter/widgets.dart' is required.
+    WidgetsFlutterBinding.ensureInitialized();
+    final database = openDatabase(
+      // Set the path to the database. Note: Using the `join` function from the
+      // `path` package is best practice to ensure the path is correctly
+      // constructed for each platform.
+      join(await getDatabasesPath(), 'doggie_database.db'),
+      // When the database is first created, create a table to store dogs.
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)",
+        );
+      },
+      // Set the version. This executes the onCreate function and provides a
+      // path to perform database upgrades and downgrades.
+      version: 1,
+    );
 
-    // Insert the Dog into the correct table. You might also specify the
-    // `conflictAlgorithm` to use in case the same dog is inserted twice.
-    //
-    // In this case, replace any previous data.
-    // await db.insert(
-    //   'dogs',
-    //   TimeTable.toMap(),
-    //   conflictAlgorithm: ConflictAlgorithm.replace,
-    // );
+    Future<void> insertDog(Dog dog) async {
+      // Get a reference to the database.
+      final Database db = await database;
+
+      // Insert the Dog into the correct table. Also specify the
+      // `conflictAlgorithm`. In this case, if the same dog is inserted
+      // multiple times, it replaces the previous data.
+      await db.insert(
+        'dogs',
+        dog.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+    }
   }
 }
---
+++ lib/model/model.dart
@@ -0,0 +1,11 @@
+class Workout {
+  final int id;
+  final String weekday;
+  final String km;
+  String time;
+  String pace;
+  String intensity;
+  String heartrate;
+
+  Workout({this.id, this.weekday, this.km});
+}

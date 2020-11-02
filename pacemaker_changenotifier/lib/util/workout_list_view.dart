09fea2657f5052c0f6ec086dfa32751c30e78294
Naskapie <naskapie@gmx.net>
2020-11-02 14:35:52 +0100

    delete pacemaker-provider

--- pacemaker_changenotifier/lib/util/workout_list_view.dart
+++ pacemaker_changenotifier/lib/util/workout_list_view.dart
@@ -10,33 +10,22 @@ class WorkoutListView extends StatelessWidget {
   WorkoutListView({this.filename, this.complete});
 
 //If workoutName =! Null load Json or already saved Workout
-//Workout erst saven wenn
 
 //If workoutName == Null, load Workout provided/set by Provider
-
-  // WorkoutsRepository workouts = LocalStorageRepository(
-  //     localStorage: KeyValueStorage(await SharedPreferences.getInstance()),
-  //     filename: filename);
 
   @override
   Widget build(BuildContext context) {
     return Selector<WorkoutListModel, List<Workout>>(
       selector: (_, model) => model.filteredWorkouts,
       builder: (context, workouts, _) {
-        // print('Selector<WorkoutListModel');
-        // print('workouts.runtimeType: ${workouts.runtimeType}');
-        // print('workouts.length: ${workouts.length}');
-
         return ListView.builder(
           itemCount: workouts.length,
           itemBuilder: (context, index) {
-            // print('ListView.builder');
             final workout = workouts[index];
             if (complete == null) {
-              // print('ComplexObjectView(workout)');
               return Column(
                 children: [
-                  ComplexObjectView(workout),
+                  if (workout.workout == filename) ComplexObjectView(workout),
                 ],
               );
             } else {
